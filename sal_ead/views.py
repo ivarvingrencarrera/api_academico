0b0cae170b2a16a04456b37043b3ec35cf525d60
raphaelrss <raphaelrss.profissional@gmail.com>
2023-01-10 15:24:07 -0300

    filter the queryset using params on the url

--- sal_ead/views.py
+++ sal_ead/views.py
@@ -7,7 +7,32 @@ from django.db.models import Q
 
 class AccountUnsyncViewSet(viewsets.ModelViewSet):
 
-    queryset = Oferta.objects.filter(
-        origem='EAD', dat_inicio__gt='2023-01-01 00:00:00', ind_excluido=0, dat_cancelamento=None)
+    queryset = Oferta.objects.all()
+    # queryset = Oferta.objects.filter(
+    #     origem='EAD', dat_inicio__gt='2023-01-01 00:00:00', ind_excluido=0, dat_cancelamento=None)
     serializer_class = AccountUnsyncSerializer
 
+
+    def get_queryset(self):
+
+        query = Q()
+
+        origem = self.request.GET.get('origem')
+        if origem is not None:
+            query.add(Q(origem=origem), Q.AND)
+
+        dat_inicio = self.request.GET.get('dat_inicio')
+        if dat_inicio is not None:
+            query.add(Q(dat_inicio__gte=dat_inicio), Q.AND)
+
+        ind_excluido = self.request.GET.get('ind_excluido')
+        if ind_excluido is not None:
+            query.add(Q(ind_excluido=ind_excluido), Q.AND)
+
+        dat_cancelamento = self.request.query_params.get('dat_cancelamento')
+        if dat_cancelamento is not None:
+            query.add(Q(dat_cancelamento=dat_cancelamento), Q.AND)
+        else:
+            query.add(Q(dat_cancelamento=None), Q.AND)
+
+        return Oferta.objects.filter(query)
